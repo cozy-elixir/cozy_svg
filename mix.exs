4efd070ba5af8375c95b42f1b6849acd2c67346a
c4710n <c4710n@users.noreply.github.com>
2024-01-14 11:46:35 +0800

    release: bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule CozySVG.MixProject do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.2.0"
   @description "A tiny and fast library to compile and render SVG files."
   @source_url "https://github.com/cozy-elixir/cozy_svg"
 
